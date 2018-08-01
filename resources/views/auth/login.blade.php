b9c72132db2286f1e33a80042d2134403a1c453a
Mohammed Siyam <m.siyam@prowareness.nl>
2018-08-01 12:30:26 +0530

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
                             <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input id="password" value="phpphp" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
