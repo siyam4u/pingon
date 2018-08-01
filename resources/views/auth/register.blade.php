d078acf65044d56c9f49006fbec36086d06517e5
Mohammed Siyam <m.siyam@prowareness.nl>
2018-08-01 12:16:25 +0530

    changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -35,7 +35,7 @@
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input value="ppp@ppp.com" id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -49,7 +49,7 @@
                             <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input value="phpphp" id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -63,7 +63,7 @@
                             <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
 
                             <div class="col-md-6">
-                                <input value="phpphp" id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <input  id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
