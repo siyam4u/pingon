9eebc05c1d9508455ceb4cca2376ec3e1a06859e
Mohammed Siyam <m.siyam@prowareness.nl>
2018-08-01 18:04:34 +0530

    Added remember me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,6 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        
                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
                                     </label>
                                 </div>
